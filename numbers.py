d0b5731006674b5738b63f329ca4283397894639
Infame <victorgdev@outlook.com>
2021-05-19 23:04:26 -0500

    Project Number version 1.1 with adds

--- numbers.py
+++ numbers.py
@@ -3,6 +3,16 @@ def printNumbers(n):
     for i in range(1,n+1):
         print(i)
     return
+def sumNumbers(n):
+    i=1;sum=0
+    for i in range(1,n+1):
+        sum+=i
+    return sum
+
 print("Program to print n numbers")
-print("Enter a number: ")
-printNumbers(int(input()))
+printNumbers(int(input("Enter a number: ")))
+num = 1
+while num > 0:
+    num = int(input("Enter number to calculate: "))
+    print(sumNumbers(num))
+print("End..")
